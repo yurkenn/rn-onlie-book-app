77b30522d42736771f444335a39fe610878b4e32
Oguz Yurken <oguz.yurken@gmail.com>
2022-11-27 01:09:10 +0300

    Fix navigation

--- src/navigation/main/MainNavigation.js
+++ src/navigation/main/MainNavigation.js
@@ -4,10 +4,10 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeNavigation from './../home/HomeNavigation';
 import AuthNavigation from './../auth/AuthNavigation';
 import {useSelector} from 'react-redux';
 import {authSelector} from '../../redux/authSlice';
+import HomeStackNavigator from '../home/HomeStackNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,8 +18,8 @@ const MainNavigation = () => {
       <Stack.Navigator>
         {isAuth ? (
           <Stack.Screen
-            name="HomeNavigation"
-            component={HomeNavigation}
+            name="HomeStackNavigator"
+            component={HomeStackNavigator}
             options={{headerShown: false}}
           />
         ) : (
